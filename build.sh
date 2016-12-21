b95ea35ac67b3494f9fb48cfd1e6bc32a3b31647
Joyee Cheung <joyeec9h3@gmail.com>
2016-12-21 12:46:09 +0800

    zesty

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t node-ubuntu -f Dockerfile-xenial .
+docker build -t node-ubuntu -f Dockerfile-zesty .
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -p 12346:12346 -p 5858:5858 -p 9229:9229 -v /Users/joyee/projects/node-ubuntu:/home/node-dev/node --security-opt seccomp=unconfined node-ubuntu:test /bin/bash
+docker run -it -p 12346:12346 -p 5858:5858 -p 9229:9229 -v /Users/joyee/projects/node-ubuntu:/home/node-dev/node --security-opt seccomp=unconfined node-ubuntu:latest /bin/bash
